4e326743998a1d49b6a8f1c6d4636b2df7a12e5a
Drazy <kexin19991018@126.com>
2020-10-13 18:23:54 +0800

    更正后台辅助方法错误说明

--- code/server/nest-framework/src/utils/base.ts
+++ code/server/nest-framework/src/utils/base.ts
@@ -4,8 +4,8 @@ const BaseUtils = {
    * 将指定时间转化成时间戳
    *
    * 通过 silly-datetime 中间件的 format 方法实现
-   * @param {datetime} data - 需要转换成为 11 位时间戳格式的 datetime 格式时间
-   * @return {number} 转换好的 11 位时间戳格式时间
+   * @param {datetime} data - 需要转换成为 13 位时间戳格式的 datetime 格式时间
+   * @return {number} 转换好的 13 位时间戳格式时间
    * @memberof ClassifyVO
    */
   transformDate: (date: any) => {
