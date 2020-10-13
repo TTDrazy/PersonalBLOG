d88d996fb8a2d5ce54012720dd5672a9e867497d
Drazy <kexin19991018@126.com>
2020-10-13 16:48:00 +0800

    增加后台跨域

--- code/server/nest-framework/dist/main.js
+++ code/server/nest-framework/dist/main.js
@@ -7,6 +7,7 @@ const swagger_1 = require("@nestjs/swagger");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     const options = new swagger_1.DocumentBuilder()
         .setTitle('博客案例')
         .setDescription('博客类型')
--- code/server/nest-framework/src/main.ts
+++ code/server/nest-framework/src/main.ts
@@ -6,6 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 处理跨域
+  app.enableCors();
+  // swagger配置
   const options = new DocumentBuilder()
     .setTitle('博客案例')
     .setDescription('博客类型')
@@ -15,6 +18,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 监听 8080 端口
   await app.listen(8080);
 }
 bootstrap();
